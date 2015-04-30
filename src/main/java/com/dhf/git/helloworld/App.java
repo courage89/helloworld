ee5d24ecba9b39d828c1dcfc9834a07418d4421e
jeremyxu <courage89@qq.com>
2015-04-30 18:19:35 +0800

    加入对App.java的第二次修改

--- src/main/java/com/dhf/git/helloworld/App.java
+++ src/main/java/com/dhf/git/helloworld/App.java
@@ -11,6 +11,9 @@ public class App
         System.out.println( "Hello World!" );
         
         System.out.println("This is the first commit...");
+        
+        System.out.println("This is the second commit...");
+        
     }
     
     
